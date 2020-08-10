a941e56e7307ec691b8de0ac56d4ac6a368e7d2e
LIFEfreedom <lifefreedom11@gmail.com>
2020-08-10 12:00:10 +0300

    add custom events

---
+++ src/Infrastructure/CustomEvents/NewObjectCreatedIntegrationEvent.cs
@@ -0,0 +1,27 @@
+﻿using LIFEfreedom.EventBusExample.Insrastructure.EventBus.Events;
+
+using System;
+using System.Text.Json.Serialization;
+
+namespace LIFEfreedom.EventBusExample.Insrastructure.EventBus.CustomEvents
+{
+	public class NewObjectCreatedIntegrationEvent : IntegrationEvent
+	{
+		public int ObjectId { get; }
+
+		public string Description { get; }
+
+		public NewObjectCreatedIntegrationEvent(int objectId, string description) : base()
+		{
+			ObjectId = objectId;
+			Description = description;
+		}
+
+		[JsonConstructor]
+		public NewObjectCreatedIntegrationEvent(Guid id, DateTime creationDate, int objectId, string description) : base(id, creationDate)
+		{
+			ObjectId = objectId;
+			Description = description;
+		}
+	}
+}
